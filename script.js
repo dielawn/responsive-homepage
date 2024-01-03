72ea82de8882c76494fd35189319142537063bba
dielawn <dielawnmerkill@gmail.com>
2024-01-03 05:30:25 -0700

    Work content iterated over creating html elements for each attribute

--- script.js
+++ script.js
@@ -1,28 +1,89 @@
 const workDiv = document.querySelector('.workDiv')
 const projects = [
     {
-        name: '',
-        discription: '',
-        screenshots: [],
+        name: 'Grocery List App',
+        discription: 'Ingredients from recipes can be compiled in to a list to shop with or share.',
+        screenshot: 'https://raw.githubusercontent.com/cashubtc/cashu-ui/2eec1685cb74ea19738709a2bb9d4a8f23dc0a90/ui/icons/svg/cashu.svg',
         url: '',
-        github: '',
+        github: 'https://github.com/dielawn/grocery-list-app',
+        id: 'listAppDiv',
     },
     {
-        name: '',
-        discription: '',
-        screenshots: [],
+        name: 'Caesar Cippher',
+        discription: 'Rot 13 Cipher. Simple text cipher used by Caesar to share secure messages with Generals. Shifting the alphabet by 13, M = A, N = B, etc... ',
+        screenshot: 'https://raw.githubusercontent.com/cashubtc/cashu-ui/2eec1685cb74ea19738709a2bb9d4a8f23dc0a90/ui/icons/svg/cashu.svg',
         url: '',
         github: '',
+        id: 'cipherAppDiv',
     },
+    {
+        name: 'Pleb Wallet',
+        discription: 'Bitcoin lightning wallet. Simple wallet leveraging the power of the LNBITS api, to send, recieve, and check transaction history.',
+        screenshot: 'https://raw.githubusercontent.com/cashubtc/cashu-ui/2eec1685cb74ea19738709a2bb9d4a8f23dc0a90/ui/icons/svg/cashu.svg',
+        url: '',
+        github: 'https://github.com/dielawn/lnbits_wallet',
+        id: 'plebWalletAppDiv',
+    },
+    {
+        name: 'Battle Ship',
+        discription: `Each player deploys his ships (of lengths varying from 2 to 5 squares) secretly on a square grid. Then each player shoots at the other's grid by calling a location. The defender responds by "Hit!" or "Miss!". You try to deduce where the enemy ships are and sink them.`,
+        screenshot: 'https://raw.githubusercontent.com/cashubtc/cashu-ui/2eec1685cb74ea19738709a2bb9d4a8f23dc0a90/ui/icons/svg/cashu.svg',
+        url: '',
+        github: 'https://github.com/dielawn/battel-ship',
+        id: 'battleShipAppDiv',
+    },
+    {
+        name: 'Chroma Salon',
+        discription: `Website for a salon in Tacoma Washington. Responsive site works and looks good on all screen sizes. Contact cards for each stylist makes booking appointments just a click away!`,
+        screenshot: 'https://raw.githubusercontent.com/cashubtc/cashu-ui/2eec1685cb74ea19738709a2bb9d4a8f23dc0a90/ui/icons/svg/cashu.svg',
+        url: '',
+        github: 'https://github.com/dielawn/chroma',
+        id: 'chromaSiteDiv',
+    },
+    {
+        name: 'Lightning Arcade',
+        discription: `Simple javascript games behind a lightning paywall. Just 25 sats to play any game! Games: Pong, Rock Paper Scissors, Battleship etc...`,
+        screenshot: 'https://raw.githubusercontent.com/cashubtc/cashu-ui/2eec1685cb74ea19738709a2bb9d4a8f23dc0a90/ui/icons/svg/cashu.svg',
+        url: '',
+        github: 'https://github.com/dielawn/lightning-arcade',
+        id: 'lightningArcadeAppDiv',
+    },
+
+
 
 ]
 
-class Project {
-    constructor(name, discription, url, github) {
-        this.name = name,
-        this.discription = discription,
-        this.screenshots = [],
-        this.url = url,
-        this.github = github,
-    }
+
+
+const displayProjectCards = () => {
+    projects.forEach(project => {
+       
+        const cardDiv = document.createElement('div')
+        cardDiv.classList.add('card')
+        cardDiv.id = project.id
+
+        const imgEl = document.createElement('img')
+        imgEl.src = project.screenshot
+
+        const projHeader = document.createElement('h3')
+        projHeader.classList.add('projHeader')
+        projHeader.textContent = project.name
+
+       
+        const githubLink = document.createElement('a')
+        githubLink.href = project.github
+        const weblink = document.createElement('a')
+        weblink.href = project.url
+        const discTxt = document.createElement('p')
+        discTxt.innerText = project.discription
+
+        cardDiv.appendChild(imgEl)
+        cardDiv.appendChild(projHeader)
+        cardDiv.appendChild(githubLink)
+        cardDiv.appendChild(weblink)
+        cardDiv.appendChild(discTxt)
+
+        document.querySelector('.workDiv').appendChild(cardDiv)
+    })
 }
+displayProjectCards()
